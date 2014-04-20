292ee0b24742596439f129ebb34ba147f0222ba0
kaalpurush <kaalbd@gmail.com>
2014-04-20 16:40:58 +0600

    fix

--- libraries/RouteSms.js
+++ libraries/RouteSms.js
@@ -1,14 +1,15 @@
 var request = require('request');
+var querystring = require('querystring');
 
 var RouteSms=function(api_key,api_secret){
 	this.end_point='http://smsplus.routesms.com:8080/bulksms/bulksms?';
 	this.api_key=api_key;
 	this.api_secret=api_secret;
 	this.send=function (message, callback){
-        var data={ username: this.api_key, password: this.api_secret, source: message.from, destination: message.to, message: message.body };
+        var data={ username: this.api_key, password: this.api_secret, type: '0', dlr: '0', source: message.from, destination: message.to, message: message.body };
 		request.get(this.end_point+querystring.stringify(data), function(error, response, body){
 			console.log(body);
-            if(body.indexOf('1701')!==false)
+            if(body.indexOf('1701')>-1)
                 callback(1);
             else
                 callback(0);			
